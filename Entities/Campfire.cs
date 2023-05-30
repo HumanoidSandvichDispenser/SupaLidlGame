323dcbebd6c32375cd163f055aab04cd2305b775
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2023-05-30 01:35:29 -0700

    campfire flashing

--- Entities/Campfire.cs
+++ Entities/Campfire.cs
@@ -5,5 +5,20 @@ namespace SupaLidlGame.Entities
 {
     public partial class Campfire : StaticBody2D
     {
+        private PointLight2D _light;
+
+        [Signal]
+        public delegate void OnCampfireUse();
+
+        public override void _Ready()
+        {
+            _light = GetNode<PointLight2D>("PointLight2D");
+        }
+
+        public override void _Process(double delta)
+        {
+            _light.Energy += (GD.Randf() - 0.5f) * 8 * (float)delta;
+            _light.Energy = Math.Clamp(_light.Energy, 1.2f, 2.0f);
+        }
     }
 }
