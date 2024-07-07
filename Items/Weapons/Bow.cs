12767e3c70955605a60f4782926b1c43fa988522
HumanoidSandvichDispenser <25856867+HumanoidSandvichDispenser@users.noreply.github.com>
2024-07-06 22:51:09 -0700

    Merge branch 'wip' into master (#42)
    * Implement igniting bow arrows
    
    * Add bow alt-fire ability

--- Items/Weapons/Bow.cs
+++ Items/Weapons/Bow.cs
@@ -8,19 +8,33 @@ public partial class Bow : ProjectileSpawner
 
     protected Area2D _ignitionArea;
 
-    protected override void SpawnProjectile(Scenes.Map map,
+    protected override Entities.Projectile SpawnProjectile(Scenes.Map map,
         Vector2 direction, float velocityModifier = 1)
     {
-        base.SpawnProjectile(map, direction, velocityModifier);
+        var projectile = base.SpawnProjectile(map, direction, velocityModifier);
+
+        if (_isOnFire)
+        {
+            GetNode<AnimatedSprite2D>("%Flame").Visible = false;
+            // TODO: instead of doing 1.5x damage, create an "On Fire" debuff
+            projectile.Hitbox.Damage *= 1.5f;
+            _isOnFire = false;
+        }
+
+        return projectile;
     }
 
     public override void _Ready()
     {
         base._Ready();
         _ignitionArea = GetNode<Area2D>("IgnitionArea");
-        _ignitionArea.AreaEntered += (Area2D area) =>
+        var onAreaEntered = (Area2D area) =>
         {
-            _isOnFire = false;
+            GetNode<AnimatedSprite2D>("%Flame").Visible = true;
+            _isOnFire = true;
         };
+        _ignitionArea.Connect(
+            Area2D.SignalName.AreaEntered,
+            Callable.From(onAreaEntered));
     }
 }
